4bedfef5a4f5dc10b64fc644ded0f441bda9fc2a
Kiyoshi Araki <kiyoshi.faraki@student-cs.fr>
2024-01-17 17:00:46 +0100

    Add all initial files

---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:app1/views/camera_view.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Tflite model',
+      theme: ThemeData(
+       
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
+        useMaterial3: true,
+      ),
+      home: const CameraView(),
+    );
+  }
+}
+
